20683867a0adecf1e694fdda3a4d6b8d42d3db38
FabianRaja <fabiraja21052002@gmail.com>
2023-12-02 12:38:10 +0530

    login and signup authentication layer implemented

--- Node/application/Routes/user.js
+++ Node/application/Routes/user.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { registerUser } from "../Controllers/user.js";
+import { getUser, registerUser } from "../Controllers/user.js";
+import bcrypt from "bcrypt";
 
 const router=express.Router();
 
@@ -11,18 +12,22 @@ router.post("/signup",async(req,res)=>{
         if(Object.keys(req.body).length<=0){
             return res.status(400).json({error:"Invalid request"})
         }
+        const salt=await bcrypt.genSalt(10);
         //if the user already registered
         const checkUser=await getUser(req.body.email);
         if(!checkUser){
-            const user=await registerUser(req.body);
-        if((user).acknowledged){
+            const hashedpass=await bcrypt.hash(req.body.password,salt);
+            const encryptUser=await {...req.body,password:hashedpass};
+            const user=await registerUser(encryptUser);
+        if(!user.acknowledged){
             return res.status(400).json({error:"Failed registration"});
         }
-        res.status(201).json({message:"register successfully",data:user})
+        return res.status(201).json({message:"registered successfully",data:user})
         }
         res.status(400).json({error:"User already exist"})
        
     } catch (error) {
+        console.log(error);
         res.status(500).json({error:'Internal server error',errorMessage:error});
     }
 })
@@ -30,9 +35,20 @@ router.post("/signup",async(req,res)=>{
 
 //login
 
-router.post("/login",(req,res)=>{
+router.post("/login",async(req,res)=>{
     try {
-        
+        if(Object.keys(req.body).length<=0){
+            return res.status(400).json({error:"Invalid request"});
+        }
+        const checkUser=await getUser(req.body.email);
+        if(!checkUser){
+            return res.status(404).json({error:"Invalid email"});
+        }
+        const validPassword=await bcrypt.compare(req.body.password,checkUser.password);
+        if(!validPassword){
+            return res.status(400).json({error:"Invalid Password"});
+        }
+        res.status(200).json({message:"Loggin in successfully"});
     } catch (error) {
         res.status(500).json({error:'Internal server error',errorMessage:error});
     }
